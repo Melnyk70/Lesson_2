080487ea847a292b77f7d37e7f3599873fe6d194
MelnykIhor <melnyk70@gmail.com>
2024-09-28 17:00:01 +0300

    заняття 2 дом завдання 1 Signed-off-by: MelnykIhor <melnyk70@gmail.com>
    Signed-off-by: MelnykIhor <melnyk70@gmail.com>

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 # This is a sample Python script.
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-user_input = input("Введіть пʼятизначне ціле додатне число:")  # Нехай користувач надрукував 4
+user_input = input("Введіть пʼятизначне ціле додатне число: ")  # Запит на ввід числа
 print(type(user_input))  # <class 'str'>
 # print(user_input / 2)  # TypeError
 number = int(user_input)
+print("Число антипод таке:", end=' ')  # Число антипод
 print(number % 10, end='')  # 5 цифра
 print(number % 100 // 10, end='')  # 4 цифра
 print(number % 1000 // 100, end='')  # 3 цифра
 print(number % 10000 // 1000, end='')  # 2 цифра
 print(number // 10000)  #1 цифра
+#https://github.com/Melnyk70/Lesson_2.git
