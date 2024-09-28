3b7641797d161cabbad4d7d2903b8f7a243d2fb7
MelnykIhor <melnyk70@gmail.com>
2024-09-28 14:25:01 +0300

    lesson2.2 zminy Signed-off-by: MelnykIhor <melnyk70@gmail.com>
    Signed-off-by: MelnykIhor <melnyk70@gmail.com>

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # This is a sample Python script.
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-
+print("Внесіть чотирьохзначне число:", end=" ")
 user_input = input()  # Нехай користувач надрукував 4
 print(type(user_input))  # <class 'str'>
 print(user_input / 2)  # TypeError
