8f79bfcb0377c037e6fa4a6148639293cca8aa91
MelnykIhor <melnyk70@gmail.com>
2024-09-28 15:05:31 +0300

    lesson2.2 zminy Signed-off-by: MelnykIhor <melnyk70@gmail.com>
    Signed-off-by: MelnykIhor <melnyk70@gmail.com>

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 # This is a sample Python script.
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-print("Внесіть чотирьохзначне число:", end=" ")
-user_input = input()  # Нехай користувач надрукував 4
+user_input = input("Введіть пʼятизначне ціле число:")  # Нехай користувач надрукував 4
 print(type(user_input))  # <class 'str'>
-print(user_input / 2)  # TypeError
+# print(user_input / 2)  # TypeError
 number = int(user_input)
-print(number / 2)  # Виведе 2
+print(number % 10, end='')  # 5 цифра
+print(number % 100 // 10, end='')  # 4 цифра
+print(number % 1000 // 100, end='')  # 3 цифра
+print(number % 10000 // 1000, end='')  # 2 цифра
+print(number // 10000)  #1 цифра
