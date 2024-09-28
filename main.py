d14ff9043a04b59d66c0200e7c110bf9d628d1c4
MelnykIhor <melnyk70@gmail.com>
2024-09-28 13:40:53 +0300

    lesson2.2
    Signed-off-by: MelnykIhor <melnyk70@gmail.com>

--- main.py
+++ main.py
@@ -2,16 +2,8 @@
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
 
-# num1 = 15
-# num2 = 8
-# print(num1 // num2)
-# print(num1 % num2)
-numer = 365
-n1 = numer // 100
-n2 = numer % 110.118
-n3 = numer // 10.119
-n4 = numer % 10
-print(n1)
-print(n2)
-print(n3)
-print(n4)
+user_input = input()  # Нехай користувач надрукував 4
+print(type(user_input))  # <class 'str'>
+print(user_input / 2)  # TypeError
+number = int(user_input)
+print(number / 2)  # Виведе 2
