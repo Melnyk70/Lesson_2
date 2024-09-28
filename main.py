d21f1a910fcc6d5f6f928a23889f044a2aebedc8
MelnykIhor <melnyk70@gmail.com>
2024-09-28 15:09:15 +0300

    lesson2.2 zminy Signed-off-by: MelnykIhor <melnyk70@gmail.com>
    Signed-off-by: MelnykIhor <melnyk70@gmail.com>

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # This is a sample Python script.
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
-user_input = input("Введіть пʼятизначне ціле число:")  # Нехай користувач надрукував 4
+user_input = input("Введіть пʼятизначне ціле додатне число:")  # Нехай користувач надрукував 4
 print(type(user_input))  # <class 'str'>
 # print(user_input / 2)  # TypeError
 number = int(user_input)
