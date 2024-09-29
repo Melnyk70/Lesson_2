7b3ebe22c89942051767d5d09b1c6aa8c0a2f48f
MelnykIhor <melnyk70@gmail.com>
2024-09-29 20:24:56 +0300

    Melnyk Igor ДЗ 2.1
    Signed-off-by: MelnykIhor <melnyk70@gmail.com>

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 # Мельник Ігор - ДЗ 2.1. Виведення числа в стовпчик
 user_input = input("Введіть чотирьохзначне ціле додатне число: ")  # Запит на ввід числа
-# print(type(user_input))  # <class 'str'>
-# print(user_input / 2)  # TypeError
 number = int(user_input)
 print("Вивід цифр числа по рядках:")  # число
 print(number // 1000)  # 1 цифра
