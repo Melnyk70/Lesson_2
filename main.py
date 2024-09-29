25e8d650a82feabbcf422f6cf5e3298722f16123
MelnykIhor <melnyk70@gmail.com>
2024-09-29 20:13:42 +0300

    ДЗ 2.1
    Signed-off-by: MelnykIhor <melnyk70@gmail.com>

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
-# Мельник Ігор - ДЗ 2.2. Необхідно "перевернути" 5-ти значне число
-user_input = input("Введіть пʼятизначне ціле додатне число: ")  # Запит на ввід числа
+# Мельник Ігор - ДЗ 2.1. Виведення числа в стовпчик
+user_input = input("Введіть чотирьохзначне ціле додатне число: ")  # Запит на ввід числа
+# print(type(user_input))  # <class 'str'>
+# print(user_input / 2)  # TypeError
 number = int(user_input)
-print("Число антипод таке:", end=' ')  # Число антипод
-print(number % 10, end='')  # 5 цифра
-print(number % 100 // 10, end='')  # 4 цифра
-print(number % 1000 // 100, end='')  # 3 цифра
-print(number % 10000 // 1000, end='')  # 2 цифра
-print(number // 10000)  #1 цифра
+print("Вивід цифр числа по рядках:")  # число
+print(number // 1000)  # 1 цифра
+print(number % 1000 // 100)  # 2 цифра
+print(number % 100 // 10)  # 3 цифра
+print(number % 10)  # 4 цифра
